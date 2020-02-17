54ae4bd037a7ee0a78b1ae36b4405d95b046bad9
bankkeez <sethitikul@gmail.com>
2020-02-17 13:37:30 +0700

    fix village area

--- model/village.js
+++ model/village.js
@@ -5,7 +5,7 @@ class Village {
         const fenceLength = 3.14 * ((Math.sqrt(property.area_input.totalArea * 4)/(22/7))) * 2;
         const sellArea = property.area_input.totalArea * 0.65;
         const roadSize = property.area_input.totalArea * 0.3;
-        const greenArea = property.area_input.totalArea * 0.3;
+        const greenArea = property.area_input.totalArea * 0.05;
         const area = {
             totalArea : 1000,
             fenceLength : fenceLength,
